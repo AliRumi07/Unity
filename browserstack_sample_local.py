4e6ea277b2debdf55e60c4297fc4fbc861c18035
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2025-03-22 00:00:33 +0500

    Add files via upload

--- browserstack_sample_local.py
+++ browserstack_sample_local.py
@@ -1,19 +1,14 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Set Appium capabilities
-options = UiAutomator2Options().load_capabilities({
-    "deviceName": "Google Pixel 3",
-    "platformName": "android",
-    "platformVersion": "9.0",
-})
 
-# Start Appium session
 driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", options=options)
 
 # Wait for 60 seconds
 time.sleep(60)
 
-# Quit driver
 driver.quit()
