cc3df302290d81c2f9d4c5a67cc357bacb4fd6b2
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2025-03-22 06:17:18 +0500

    Update browserstack.py

--- browserstack.py
+++ browserstack.py
@@ -1,44 +1,24 @@
-from flask import Flask
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-import threading
 
-app = Flask(__name__)
 
-# Flag to check if script is running
-script_running = True
+options = UiAutomator2Options().load_capabilities({
+    # Specify device and os_version for testing
+    "deviceName": "Google Pixel 3",
+    "platformName": "android",
+    "platformVersion": "9.0",
 
-@app.route('/')
-def status():
-    return "Script is running" if script_running else "Script has stopped"
+})
 
-def run_appium_script():
-    global script_running
-    try:
-        options = UiAutomator2Options().load_capabilities({
-            "deviceName": "Google Pixel 3",
-            "platformName": "android",
-            "platformVersion": "9.0",
-            
-        })
+driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", options=options)
 
-        driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", options=options)
+# Wait for 1 seconds
+time.sleep(1)
 
-        # Wait for 10 seconds
-        time.sleep(1)
 
-        # Invoke driver.quit() after the test is done to indicate that the test is completed.
-        driver.quit()
-    except Exception as e:
-        print(f"Error occurred: {e}")
-    finally:
-        # Schedule the script to run again after 2 minutes
-        threading.Timer(10000, run_appium_script).start()
-
-if __name__ == '__main__':
-    # Run Appium script for the first time
-    threading.Thread(target=run_appium_script, daemon=True).start()
-
-    # Start Flask server
-    app.run(host="0.0.0.0", port=8080)
+# Invoke driver.quit() after the test is done to indicate that the test is completed.
+driver.quit()
